85c2f0b8c0135f122cc2bf1ec6f2ebb60eb4f96f
Lukas <lukasjonkers@hotmai.com>
2021-05-14 13:00:43 +0200

    minor edits

--- sizePattern.R
+++ sizePattern.R
@@ -15,6 +15,9 @@ sizeInt <- dat %>%
   mutate(cumuConcentration = cumsum(totalConcentration),
          s = cumuConcentration/first(cumuConcentration))
 
+sizeInt %>%
+  ggplot(aes(lon, lat)) +
+  geom_point()
 
 p1 <- sizeInt %>%
   filter(is.finite(s)) %>%
@@ -31,16 +34,20 @@ p1 <- sizeInt %>%
 
 p2 <- sizeInt %>%
   filter(is.finite(s)) %>%
-  filter(lat > 30) %>%
-  ggplot(aes(size_fraction_min, s, colour = log10(sample_min_depth + 1))) +
+  mutate(depth_bin = cut(sample_min_depth, include.lowest = TRUE, breaks = c(0, 50, 100, 250, 500, 5000)),
+         lon_bin = cut(lon, include.lowest = TRUE, breaks = c(-100, -40, 35, 80)),
+         lat_bin = cut(lat, include.lowest = TRUE, breaks = c(0, 30, 60, 80))) %>%
+  # View()
+  ggplot(aes(size_fraction_min, s)) +
   # ggplot(aes(size_fraction_min, s, colour = lat)) +
   # ggplot(aes(size_fraction_min, s, colour = lon)) +
-  geom_path(aes(group = sample_id), alpha = 0.2, size = 1) +
-  geom_point(aes(group = sample_id), alpha = 0.2) +
+  geom_path(aes(group = sample_id), alpha = 0.1, size = 1) +
+  geom_point(aes(group = sample_id), alpha = 0.1) +
   scale_x_log10() +
   scale_y_log10() +
-  scale_colour_viridis_c() +
-  geom_smooth(method = 'loess', colour = 'grey20', size = 3) +
+  # scale_colour_viridis_c() +
+  geom_smooth(aes(colour = depth_bin), method = 'loess', size = 2) +
+  facet_grid(vars(lat_bin), vars(lon_bin)) +
   labs(title = 'Multiplication factor') +
   theme_bw()
 
